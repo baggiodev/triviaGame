d72caf0480a5e15c4c472c6ddd29c0aefc6a2cd3
Baggio Shehadi <k3nh1n@yahoo.com>
2017-03-24 12:44:28 -0500

    added pictures

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,37 +9,44 @@ var trivia = {
         q1: {
             q: "What was the most selling game in history?",
             a: "Wii Sports",
-            c: ["Wii Sports", "Tetris", "Pong", "Super Mario Bros."]
+            c: ["Wii Sports", "Tetris", "Pong", "Super Mario Bros."],
+            img: "assets/images/wiiSports.png"
         },
         q2: {
             q: "In what year was Sega Genesis released in North America?",
             a: "1989",
-            c: ["1989", "1999", "1991", "1975"]
+            c: ["1989", "1999", "1991", "1975"],
+            img: "assets/images/segaGen.png"
         },
         q3: {
             q: "Which of the following video games takes place in a dystopian underwater city called Rapture?",
             a: "Bioshock",
-            c: ["Bioshock", "Fallout 3", "God of War", "Half-Life"]
+            c: ["Bioshock", "Fallout 3", "God of War", "Half-Life"],
+            img: "assets/images/bioshock.png"
         },
         q4: {
             q: "E. Honda, Dhalsim and Chun Li are all characters from what video game series?",
             a: "Street Fighter",
-            c: ["Street Fighter", "Tekken", "Battletoads", "Mortal Kombat"]
+            c: ["Street Fighter", "Tekken", "Battletoads", "Mortal Kombat"],
+            img: "assets/images/ryu.png"
         },
         q5: {
             q: "What classic beat-'em-up game featured brothers Billy Lee and Jimmy (also nicknamed Spike and Hammer)?",
             a: "Double Dragon",
-            c: ["Double Dragon", "Smash Brothers", "Snow Brothers", "Ninja Gaiden"]
+            c: ["Double Dragon", "Smash Brothers", "Snow Brothers", "Ninja Gaiden"],
+            img: "assets/images/doubleDrag.png"
         },
         q6: {
             q: "How many bits was the Super Nintendo Entertainment System?",
             a: "16",
-            c: ["16", "8", "128", "64"]
+            c: ["16", "8", "128", "64"],
+            img: "assets/images/snes.png"
         },
         q7: {
             q: "What character do you play as in The Legend Of Zelda?",
             a: "Link",
-            c: ["Link", "Zelda", "Gandolf", "Tingle"]
+            c: ["Link", "Zelda", "Gandolf", "Tingle"],
+            img: "assets/images/link.png"
         }
 
     },
@@ -99,7 +106,8 @@ var trivia = {
     buffer: function() {
         trivia.time = 30;
         $("#time").html("Time left: " + trivia.time);
-        $("#mainContent").html("<br><p>Answer: " + trivia.questions["q" + trivia.writeq].a + "</p><p>Correct: " + trivia.correct + "</p><p>Incorrect: " + trivia.incorrect + "</p><p>Unanswered: " + trivia.unanswered + "</p>");
+        var imgname = trivia.questions["q"+trivia.writeq].img;
+        $("#mainContent").html("<br><p>Answer: " + trivia.questions["q" + trivia.writeq].a + "</p><p>Correct: " + trivia.correct + "</p><p>Incorrect: " + trivia.incorrect + "</p><p>Unanswered: " + trivia.unanswered + "</p><br><img id='previewpic' src="+imgname+">");
         trivia.writeq++;
         if (trivia.writeq <= (trivia.oblength())) {
             setTimeout(trivia.start, 1000 * 3);
